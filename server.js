3d6e788b4c9c0bf1a7375a31c93164c061ca5a56
Stussyz <mfadhol07@gmail.com>
2026-05-17 03:42:01 +0700

    add: middleware, POST data pasienBaru

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require ('express');
 const app = express();
 const port = 3000;
 
+// Middleware - JSON reader
+app.use(express.json());
+
 // Mock database - variable list array of objects:
 const dataPasien = [
     { id:1, nama: "Budi Doremi", keluhan: "Demam Tinggi", poli: "umum" },
     { id:2, nama: "Siti tilulit", keluhan: "Sakit Gigi", poli: "Gigi" }
 ];
 
-// Endpoint: root
+// Endpoint testing: root
 app.get('/', (req, res) => {
     res.send("Endpoint root SIMRS Works!");
 });
@@ -17,6 +20,18 @@ app.get('/', (req, res) => {
 app.get('/pasien', (req, res) => {
     // Mengirim dataPasien dari mockup db ke bentuk JSON
     res.json(dataPasien);
+});
+
+// Menambah data pasien baru
+app.post('/pasien', (req, res) => {
+    // Menangkap data JSON yang dikirim dari frontend/thunder client
+    const pasienBaru = req.body;
+
+    // Input data pasien baru ke db/backend
+    dataPasien.push(pasienBaru);
+
+    // Respon setelah data berhasil diinput ke db/backend
+    res.json({ message: "Data pasien berhasil ditambahkan!", data: pasienBaru });
 });
 
 // Menyalakan server
